488c83c1116c9a3937dd2ac36f28e648b7f8b7d6
Kerolos George <119162871+Kerolos-George@users.noreply.github.com>
2025-09-05 11:21:15 +0300

    test back

--- booking/src/main.ts
+++ booking/src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap() {
   
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  
+  // await
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
